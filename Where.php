684e2dd9f90b05ba799addb00829d5e6b837fae2
gridorius <gridorius@yandex.ru>
2019-02-09 02:33:02 +0500

    where

---
+++ Where.php
@@ -0,0 +1,40 @@
+<?php
+class Where{
+  public $where = '';
+
+  public function where(){
+    $ca = func_num_args();
+    $args = func_get_args();
+
+    if(!empty($this->where))
+      $this->where.= ' AND';
+
+    $this->whereWrite($ca, $args);
+    return $this;
+  }
+
+  public function orWhere(){
+    $ca = func_num_args();
+    $args = func_get_args();
+
+    if(!empty($this->where))
+      $this->where.= ' OR';
+
+    $this->whereWrite($ca, $args);
+    return $this;
+  }
+
+  public function whereWrite($ca, $args){
+    if($ca == 1)
+      $this->where.= ' ('.func_get_args()[0](new Where())->get().')';
+    else if($ca == 2)
+      $this->where.= ' '.$args[0].' = '.$args[1];
+    else if($ca == 3)
+      $this->where.= ' '.$args[0].' '.$args[1].' '.$args[2];
+  }
+
+  public function get(){
+    return $this->where;
+  }
+}
+?>
--- index.php
+++ index.php
@@ -3,12 +3,9 @@ include_once 'Column.php';
 include_once 'Table.php';
 include_once 'Foreign.php';
 include_once 'Database.php';
+include_once 'Where.php';
 
-$database = new Database('test', 'root', '');
-$table = $database->table('user_files');
-$user_id = (new Column('user_id', 'int(3)'))->foreign(new Foreign('user_id', 'users'));
-$file_id = (new Column('file_id', 'int(3)'))->foreign(new Foreign('file_id', 'files'));
-$table->addColumns($user_id, $file_id);
-
-$table->create();
+$where = new Where();
+$where->where('name', 'gridorius')->where('password', '123');
+echo $where->get();
 ?>
