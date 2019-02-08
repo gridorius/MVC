7a834eee24343c1590601fe6cb557f4234e8b4a4
gridorius <gridorius@yandex.ru>
2019-02-09 00:08:35 +0500

    column test

--- Column.php
+++ Column.php
@@ -2,14 +2,44 @@
 class Column{
   private $name;
   private $type;
-  private $null;
-  private $key;
-  private $default;
-  private $extra;
+  private $null = false;
+  private $default = false;
+  private $increment = false;
+  private $primary = false;
+  private $unique = false;
 
   public function __construct($name, $type){
     $this->name = $name;
     $this->type = $type;
+  }
+
+  public function primary(){
+    $this->primary = true;
+  }
+
+  public function unique(){
+    $this->unique = true;
+  }
+
+  public function increment(){
+    $this->increment = true;
+  }
+
+  public function setDefault($default){
+    $this->default = $default;
+  }
+
+  public function nullable(){
+    $this->null = true;
+  }
+
+  public function create(){
+    $column = $this->name.' '.$this->type;
+    $column.= $this->null ? ' NULL' : ' NOT NULL';
+    $column.= $this->default ? " DEFAULT ".$this->default : '';
+    $column.= $this->increment ? ' AUTO_INCREMENT' : '';
+
+    return $column;
   }
 }
 ?>
