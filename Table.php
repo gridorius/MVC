8870cf812f3fa8f7c8cadbf5a47c21a4b25dc005
gridorius <gridorius@yandex.ru>
2019-02-09 00:16:34 +0500

    test table

--- Table.php
+++ Table.php
@@ -1,5 +1,21 @@
 <?php
 class Table{
-  
+  private $name;
+  private $columns = [];
+
+  public function __construct($name){
+    $this->name = $name;
+  }
+
+  public function addColumn($column){
+    $this->columns[] = $column;
+  }
+
+  public function create(){
+    $fields = [];
+    foreach($this->columns as $column)
+      $fields[] = $column->create();
+    return join(',', $fields);
+  }
 }
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
 include_once 'Column.php';
+include_once 'Table.php';
+
 
 ?>
