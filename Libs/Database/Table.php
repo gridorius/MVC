1a8d98b87575f0df9e69bc16d639fadb90ed671b
gridorius <gridorius@yandex.ru>
2019-02-09 12:28:12 +0500

    query constructor

--- Libs/Database/Table.php
+++ Libs/Database/Table.php
@@ -1,21 +1,53 @@
 <?php
-class Table{
+class Table implements IteratorAggregate{
   protected $name;
   protected $columns = [];
+  private $where;
+  private $select = false;
+  private $rows = [];
 
   public function __construct($name){
     $this->name = $name;
+    $this->where = new Where();
   }
 
   public function addColumns(){
     foreach(func_get_args() as $column)
       $this->columns[$column->name] = $column;
   }
+
+  public function createQuery(){
+    $select = $this->select ? join(', ', $this->select) : '*';
+    $table = $this->name;
+    $where = $this->where;
+    return "SELECT $select FROM $table WHERE $where";
+  }
+
+  public function getIterator(){
+    Database::execute($query);
+  }
+
+  public function where(){
+    (new ReflectionMethod('Where', 'where'))->invokeArgs($this->where, func_get_args());
+    return $this;
+  }
+
+  public function orWhere(){
+    (new ReflectionMethod('Where', 'orWhere'))->invokeArgs($this->where, func_get_args());
+    return $this;
+  }
+
+  public function select(){
+    $this->select = func_get_args();
+    return $this;
+  }
 }
+
+
 
 class NewTable extends Table{
 
-  public function createQuery(){
+  public function createTable(){
     $fields = [];
     $primary = [];
     $unique = [];
@@ -48,7 +80,7 @@ class NewTable extends Table{
   }
 
   public function create(){
-    Database::execute($this->createQuery());
+    Database::execute($this->createTable());
   }
 }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include_once 'Bootstrap.php';
 
-
+$table = new Table('users');
+echo $table->select('uer_id', 'login')->where('password', '123')->createQuery();
 ?>
